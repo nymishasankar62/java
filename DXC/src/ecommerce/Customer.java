3c9c5a340b2253adf5ba9dbb9297caae994b54a7
nymishasankar62 <92718747+nymishasankar62@users.noreply.github.com>
2021-10-19 15:12:28 +0530

    Initial commit

---
+++ DXC/src/ecommerce/Customer.java
@@ -0,0 +1,15 @@
+package ecommerce;
+
+public class Customer {
+  public void walkin() {
+	 System.out.println("swetha is going inside big bazar");
+	 
+ }
+ public void shop() {
+	 System.out.println("cust is shopping");
+ }
+ public void checkout() {
+	 Cashier RahulCashier=new Cashier();
+	 RahulCashier.estimate();
+ }
+}
